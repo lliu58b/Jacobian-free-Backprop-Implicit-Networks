6790f280727f32446c907e90d59eded7d9f805bb
lliu58b <linghai_liu@brown.edu>
2022-06-25 23:08:57 -0400

    degrad model 062522

--- functions.py
+++ functions.py
@@ -4,14 +4,19 @@ Functions for training and testing different models
 def train_jfb(model, loader, operator, loss_function, optimizer, device):
     model.train()
     L = []
+    n_iters_list = []
+    grad_norm_list = []
     for batch_idx, X in enumerate(loader):
+        print(batch_idx)
+        if batch_idx > 2:
+            break
         X = X.to(device)
-        # if batch_idx % 10 == 0:
-        #     print(f'now batch {batch_idx} is finished')
         d = operator.forward(X)
-        pred = model(d)
+        pred, n_iters = model(d)
         batch_loss = loss_function(pred, X)
         batch_loss.backward()
+        grad_norm_list.append(model.current_grad_norm())
         optimizer.step()
         L.append(batch_loss.item())
-    return L
+        n_iters_list.append(n_iters)
+    return L, n_iters_list, grad_norm_list
--- script.py
+++ script.py
@@ -5,10 +5,9 @@ from operators import *
 from metrics import *
 from functions import *
 from customdataset import *
+from DEmodels import *
 
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-data_location = './data/'
 
 # Load the data
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -36,3 +35,26 @@ A = GaussianBlur(sigma=kernel_sigma, kernel_size=kernel_size).to(device=device)
 # This maps image x* to measurement d 
 measurement_process = OperatorPlusNoise(A, noise_sigma=noise_sigma)
 
+num_channels = 3
+lossfunction = torch.nn.MSELoss(reduction='sum')
+learning_rate = 0.001
+step_size = 0.001
+num_epoch = 1
+degrad_model = DEGRAD(c=num_channels, blur_operator=A, step_size=step_size)
+degrad_model.to(device)
+optimizer = torch.optim.Adam(degrad_model.parameters(), lr=learning_rate)
+avg_loss_epoch = []
+avg_n_iters = []
+avg_grad_norm = []
+data_batch = iter(test_dataloader).next()
+for epoch in range(num_epoch):
+    epoch_loss_list, epoch_n_iters_list, grad_norm_list = train_jfb(degrad_model, train_dataloader, measurement_process, lossfunction, optimizer, device)
+    epoch_loss = np.mean(epoch_loss_list)
+    epoch_n_iters = np.mean(epoch_n_iters_list)
+    epoch_grad_norm = np.mean(grad_norm_list)
+    avg_loss_epoch.append(epoch_loss)
+    avg_n_iters.append(epoch_n_iters)
+    avg_grad_norm.append(epoch_grad_norm)
+    print(f"Epoch {epoch+1} finished, average loss: {epoch_loss:.4f}, average number of iterations: {epoch_n_iters} out of 100, average gradient norm: {epoch_grad_norm:.4f}")
+    # test_batch(degrad_model, data_batch, device)
+# plotting(avg_loss_epoch, avg_n_iters, avg_grad_norm)
